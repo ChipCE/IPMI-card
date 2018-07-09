f228af8ed76c4185a8b751dffc9f73f50b503f04
Chip <login721@gmail.com>
2018-07-09 09:16:29 +0900

    daily commit

--- firmware-v2/firmware-v2.ino
+++ firmware-v2/firmware-v2.ino
@@ -1,17 +1,36 @@
 #include "HardwareController.h"
+#include <WiFiMan.h>
 
-HardwareController hc;
+//Global
+WiFiMan wman;
+
 
 void setup()
 {
-    hc = HardwareController();
+    //init serial , may not needed , WiFiMan will do it
     Serial.begin(115200);
-    hc.setLed(true);
-    hc.blinkLed(200,15000);
+
+    //init
+    WiFiMan wman = WiFiMan(true,true,true);
+
+    //wait for force config mode , 1 click
+
+    //wait for rest config , hold
+
+    //start wman
+    wman.start();
+
+    //if connected , run mqtt connect
 }
+
+
+//MQTT handle 
+    //if serialLine -> decode it
+    //if mqtt.mes -> decode it
+
+
 
 void loop()
 {
-    
-    hc.handleHardware();
+    //mqtt handle
 }
