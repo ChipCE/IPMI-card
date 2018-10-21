e0f6a92254c7b2fa736a1217370d3e93a1d45a8f
Chip <login721@gmail.com>
2018-10-21 12:42:47 +0900

    daily commit

--- std/ipmi.sh
+++ std/ipmi.sh
@@ -4,8 +4,16 @@ version="1.0 alpha 1018-10-21"
 # function
 ipmi-help () {
     echo "Generic IPMI std $version"
-    echo "usage : ipmi [help] [version] [status] [shell] [power-on] [power-off] [reboot] [force-shutdown]"
-    echo -e "\tTest"
+    echo -e "USAGE \n\t ipmi [help] [version] [status] [shell] [power-on] [power-off] [reboot] [-f]"
+    echo "OPTIONS"
+    echo -e "\t help \n\t\t Print a short help text and exit."
+    echo -e "\t version \n\t\t Display application version."
+    echo -e "\t status \n\t\t Display system current status."
+    echo -e "\t shell \n\t\t Connect to system via ssh."
+    echo -e "\t power-on \n\t\t Power-on the system."
+    echo -e "\t power-off \n\t\t Power-off(shutdown) the system."
+    echo -e "\t reboot \n\t\t Reboot the system."
+    echo -e "\t -f \n\t\t Force power-off or reboot the system."
 }
 
 ipmi-version () {
@@ -15,7 +23,6 @@ ipmi-version () {
 
 
 # main program
-echo "#DEBUG arg1 = $1"
 # check if the first arg is empty
 if [[ -z "$1" ]]; then
    ipmi-help
@@ -40,15 +47,33 @@ else
     fi
 
     # handle power-on
-
+    if [ "$1" = "power-on" ]; then
+        echo "power-on"
+        exit 0
+    fi
 
     # handle power-off
+    if [ "$1" = "power-off" ]; then
+        if [ "$2" = "-f" ]; then
+            echo "force shutdown"
+            exit 0
+        else
+            echo "shutdown"
+            exit 0
+        fi
+    fi
 
 
     # handle reboot
-
-
-    # handle force-shutdown
+    if [ "$1" = "reboot" ]; then
+        if [ "$2" = "-f" ]; then
+            echo "force reboot"
+            exit 0
+        else
+            echo "reboot"
+            exit 0
+        fi
+    fi
 
     # help
     if [ "$1" = "help" ]; then
