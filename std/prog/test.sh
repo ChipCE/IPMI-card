3f08042c04fbafa255905a33468038ef6d51e88a
Chip <login721@gmail.com>
2018-11-03 23:25:33 +0900

    updated grep script

--- std/prog/test.sh
+++ std/prog/test.sh
@@ -5,7 +5,7 @@ networkAddr="192.168.1.0/24"
 
 # get current ip address
 echo "Network interface : $netIface"
-localIP=$(ifconfig $netIface | grep -Po "inet\s[\d]+\.[\d]+\.[\d]\.[\d]+" | grep -Po "[\d]+\.[\d]+\.[\d]\.[\d]+")
+localIP=$(ifconfig $netIface | grep -Po "inet\s\d+\.\d+\.\d\.\d+" | grep -Po "\d+\.\d+\.\d\.\d+")
 echo "Local IP address : $localIP"
 
 # delete old tmp file if needed
@@ -15,13 +15,16 @@ fi
 
 echo "Scan for device(s) in $networkAddr with nmap"
 nmap $networkAddr >> tmp.txt
-result=$(grep -Po "[\d]+\.[\d]+\.[\d]+\.[\d]+" tmp.txt | grep -v "$localIP" | head -1)
+result=$(grep -Po "\d+\.\d+\.\d+\.\d+" tmp.txt | grep -v "$localIP" | head -1)
 echo "Found host IP address : $result"
 
 if [ -f ./ipmi.conf ]; then
     rm ./ipmi.conf
 fi
+
+echo "Writing config file..."
 echo "$result" >> ipmi.conf
+echo "Done!"
 
 # delete tmp file
 rm ./tmp.txt
